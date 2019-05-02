65bc0d1d26af56a8e5f522d328644f190ba418ca
Freeze88 <andrevitorino1997@hotmail.com>
2019-05-02 16:08:11 +0100

    Creation of a program that swithces between 2 players and shows what they have, and a class containing the type and number of ghost each player has

---
+++ LP1/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LP1
+{
+    class Player
+    {
+        private int blue_ghost;
+        private int red_ghost;
+        private int yellow_ghost;
+
+        public Player(int blue, int red, int yellow)
+        {
+            this.blue_ghost = blue;
+            this.red_ghost = red;
+            this.yellow_ghost = yellow;
+        }
+
+        public int GetGhostsBlue()
+        {
+            return blue_ghost;
+        }
+        public int GetGhostsRed()
+        {
+            return red_ghost;
+        }
+        public int GetGhostsYellow()
+        {
+            return yellow_ghost;
+        }
+        public void Update(int blue, int red, int yellow)
+        {
+            this.blue_ghost = blue;
+            this.red_ghost = red;
+            this.yellow_ghost = yellow;
+            
+        }
+    }
+}
--- LP1/Program.cs
+++ LP1/Program.cs
@@ -6,7 +6,41 @@ namespace LP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int blue = 3;
+            int red= 3;
+            int yellow = 3;
+
+            int nPlayer = 0;
+
+            Player[] players = new Player[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                Player player = new Player(blue, red, yellow);
+
+                players[i] = player;
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "a")
+                {
+                    Console.WriteLine(players[nPlayer].GetGhostsBlue());
+                    Console.WriteLine(players[nPlayer].GetGhostsRed());
+                    Console.WriteLine(players[nPlayer].GetGhostsYellow());
+                }
+                if (input == "b")
+                {
+                    blue--;
+
+                    players[nPlayer].Update(blue, red, yellow);
+                }
+
+                if (nPlayer == 0) nPlayer = 1;
+                else nPlayer = 0;
+            }
         }
     }
 }
