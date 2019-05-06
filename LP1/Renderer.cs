7ac4c66b847ee82a63f20d9caa849d243738ae42
Lagostaz <carol1999b@gmail.com>
2019-05-06 18:46:16 +0100

    Add Renderer class

---
+++ LP1/Renderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Renderer
+{
+    public void Render(Board board)
+	{
+        char[,] symbols = new char[5, 5];
+        
+        for (int row = 0; row < 5; row++)
+            for (int column = 0; column = 5; column++)
+                symbols[row, column] = SymbolFor(board.GetState(new Position(row, column)));
+
+        Console.WriteLine()
+
+	}
+}
+
